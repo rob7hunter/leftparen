9e5c92efcae2cc3eb2f67053464135675f42ed9e
Bill Hager <whager@gmail.com>
2008-10-18 16:49:26 -0400

    Uploaded files now retain their filename are saved in a private location.  Added additional error checking for user submitted data.

---
+++ files.scm
@@ -0,0 +1,23 @@
+#lang scheme/base
+
+(require (file "util.scm")
+         (file "web-support.scm")
+         "settings.scm")
+
+(provide save-uploaded-file!)
+
+(declare-setting *PATH_TO_FILEDATA* (build-path (current-directory) "filedata"))
+
+(define (save-uploaded-file! file-data)
+  (let* ((filename (fresh-filename-id (binding/string:file-filename file-data)))
+        (raw-file-bytes (binding/string:file-content file-data))
+        (fport (open-output-file (build-path (setting *PATH_TO_FILEDATA*) filename) #:exists 'error)))
+    (write-bytes raw-file-bytes fport)
+    (close-output-port fport)
+    filename))
+
+(define (fresh-filename-id filename #:id-length (id-length 5))
+  (let ((try (string-append (random-key-string id-length) "-" filename)))
+    (if (file-exists? (build-path "../filedata" try))
+        (fresh-filename-id filename #:id-length id-length)
+        try)))
